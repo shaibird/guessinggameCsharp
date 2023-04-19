1de03879a30b936de5ab62716978f19f47264f6e
Shaina Couch <shainacouch21@gmail.com>
2023-04-19 17:24:34 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        FavNumberQuestion();
+    }
+
+    static void FavNumberQuestion()
+    {
+        int magicNumber = 45;
+
+        Console.Write("Can you guess the magic number?");
+        string answer = Console.ReadLine();
+        int parsedInput = int.Parse(answer);
+        
+        if (parsedInput == magicNumber)
+        { 
+            Console.WriteLine("Yayy! You guessed it!");
+        }
+        else
+        {
+            Console.WriteLine("Ha. SO wrong.");
+        }
+    }
+}
