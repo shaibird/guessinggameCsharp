8da979060456288ad4fd7161befd910b34ba94db
Shaina Couch <shainacouch21@gmail.com>
2023-04-24 10:48:50 -0500

    changed so player gets 4 guesses

--- Program.cs
+++ Program.cs
@@ -8,20 +8,35 @@ class Program
     }
 
     static void FavNumberQuestion()
+{
+    int magicNumber = 45;
+    int numAttempts = 4;
+    
+    for (int i = 0; i < numAttempts; i++)
     {
-        int magicNumber = 45;
-
-        Console.Write("Can you guess the magic number?");
+        Console.Write("Can you guess the magic number? ");
         string answer = Console.ReadLine();
-        int parsedInput = int.Parse(answer);
+        int parsedInput;
+
+        if (!int.TryParse(answer, out parsedInput))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+            i--;
+            continue;
+        }
         
         if (parsedInput == magicNumber)
         { 
             Console.WriteLine("Yayy! You guessed it!");
+            return;
         }
         else
         {
-            Console.WriteLine("Ha. SO wrong.");
+            Console.WriteLine("Wrong answer. Try again.");
         }
     }
+
+    Console.WriteLine("You have run out of attempts. The magic number was {0}.", magicNumber);
+}
+
 }
